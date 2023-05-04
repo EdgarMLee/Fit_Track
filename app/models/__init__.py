e9ea73b3fa84f05fd9ec18266bff1bed53eecf1f
edgar <edgarlee135@gmail.com>
2023-05-04 16:55:54 -0700

    init models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,7 @@
 from .db import db
 from .user import User
+from .exercises import Exercise
+from .plans import Plan
+from .reviews import Review
+from .sets import Set
+from .workouts import Workout

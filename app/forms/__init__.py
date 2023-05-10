2a62912be818c947a3b25909caa6d004cf120555
edgar <edgarlee135@gmail.com>
2023-05-10 14:02:54 -0700

    init forms

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,2 +1,6 @@
 from .login_form import LoginForm
 from .signup_form import SignUpForm
+from .plan_form import PlanForm
+from .review_form import ReviewForm
+from .set_form import SetForm
+from .workout_form import WorkoutForm

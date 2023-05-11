5a23e8b93a0e2e505bf1fe62a79d094acbeddf17
edgar <edgarlee135@gmail.com>
2023-05-11 08:43:25 -0700

    comments to plan routes

--- app/api/plan_routes.py
+++ app/api/plan_routes.py
@@ -9,23 +9,31 @@ plan_routes = Blueprint("plans", __name__, url_prefix="/plans")
 #Get all Plans
 @plan_routes.route("")
 def all_plans():
+    #Query for all plans
     plans = Plan.query.all()
+    #Empty array to store queried plans
     planarr = []
     if plans is not None:
+        #If plans exists, create loop and convert object to dictionary
         for plan in plans:
             plan = plan.to_dict()
             # planId = plan.to_dict()["id"]
             # image = db.session.query(Image).filter(Image.planId == planId).first()
             # if image:
             #     plan['image'] = image.to_dict()
+            #append dictionary to array
             planarr.append(plan)
+    #Return as a json response
     return {"plans": planarr}
 
 #Get Plan by ID
 @plan_routes.route("/<int:id>")
 def get_plan(id):
+    #Query for plan based on ID
     plan = Plan.query.get(id)
+    #Empty array to store queried plan
     planarr = []
+    #If plan exists, convert object to dictionary
     if plan is not None:
         plan = plan.to_dict()
         # planId = plan.to_dict()["id"]
@@ -34,6 +42,7 @@ def get_plan(id):
         # ^ Input that once you added seeder data to image for 2nd image
         # if image:
         #     plan["image"] = image.to_dict()
+        #Append plan to array
         planarr.append(plan)
     return plan
 
@@ -59,4 +68,6 @@ def create_plan():
         return jsonify(new_plan.to_dict()), 200
     else:
         return {"errors": validation_errors_to_error_messages(form.errors)}, 401
+
+#Edit a Plan
 

aa612ec537874d086abd1350e6e9088a2b7f53f8
edgar <edgarlee135@gmail.com>
2023-05-11 09:00:31 -0700

    comments to plan routes

--- app/api/plan_routes.py
+++ app/api/plan_routes.py
@@ -50,12 +50,17 @@ def get_plan(id):
 @plan_routes.route("/", methods=["POST"])
 @login_required
 def create_plan():
+    #Create instance of PlanForm class
     form = PlanForm()
+    #Sets CSRF Token in form as security to prevent cross-site request forgery attacks
     form["csrf_token"].data = request.cookies["csrf_token"]
+    #Queries for all exercises from database
     exercises = Exercise.query.all()
+    #Sets choices for exercise form
     form.exercise.choices=[(exercise.to_exercise(), exercise.to_exercisename()) for exercise in exercises]
     if form.validate_on_submit():
         new_plan = Plan(
+            #Input fields with info
             owner_id=current_user.id,
             name=form.name.data,
             private=form.private.data,
@@ -63,8 +68,10 @@ def create_plan():
             exercises=form.exercises.data,
             workouts=form.workouts.data
         )
+        #Add and save changes to database
         db.session.add(new_plan)
         db.session.commit()
+        #Return jsonified data with successful status
         return jsonify(new_plan.to_dict()), 200
     else:
         return {"errors": validation_errors_to_error_messages(form.errors)}, 401
@@ -73,21 +80,30 @@ def create_plan():
 @plan_routes.route("/<int:plan_id>", methods=["PUT"])
 @login_required
 def edit_plan(plan_id):
+  #Create instance of PlanForm class
   form = PlanForm()
+  #Sets CSRF Token in form as security to prevent cross-site request forgery attacks
   form["csrf_token"].data = request.cookies["csrf_token"]
+  #Queries for all exercises from database
   exercises = Exercise.query.all()
+  #Sets choices for exercise form
   form.exercise.choices=[(exercise.to_exercise(), exercise.to_exercisename()) for exercise in exercises]
   if form.validate_on_submit():
+      #Queries for plan based on plan ID
       plan = Plan.query.get(plan_id)
+      #Authorization to check if plan belongs to owner before allowing update
       if plan.owner_id == current_user.id:
           plan.name = form.name.data
           plan.private = form.private.data
           plan.time = form.time.data
           plan.exercises = form.exercises.data
           plan.workouts = form.workouts.data
+          #Save changes to database
           db.session.commit()
+          #Returns updated plan as JSON with status code 204
           return jsonify(plan.to_dict()), 204
       else:
+          #If current user is not authorized to modify plan
           return {"errors": "Unauthorized"}, 401
   else:
       return {"errors": validation_errors_to_error_messages(form.errors)}, 401

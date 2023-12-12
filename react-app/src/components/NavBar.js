5326614b6b54fc6cea0d2dcbcea894d3647f0177
edgar <edgarlee135@gmail.com>
2023-12-11 16:12:15 -0800

    added modal, docker file

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,38 +1,46 @@
-
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import LogoutButton from './auth/LogoutButton';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import LogoutButton from "./auth/LogoutButton";
+import { useSelector } from "react-redux";
 
 const NavBar = () => {
+  const sessionUser = useSelector((state) => state.session.user);
+
   return (
     <nav>
       <ul>
         <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
+          <NavLink to="/" exact={true} activeClassName="active">
             Home
           </NavLink>
         </li>
+        {!sessionUser && (
+          <>
+            <li>
+              <NavLink to="/login" exact={true} activeClassName="active">
+                Login
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/sign-up" exact={true} activeClassName="active">
+                Sign Up
+              </NavLink>
+            </li>
+          </>
+        )}
         <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
+          <NavLink to="/users" exact={true} activeClassName="active">
             Users
           </NavLink>
         </li>
-        <li>
-          <LogoutButton />
-        </li>
+        {sessionUser && (
+          <li>
+            <LogoutButton />
+          </li>
+        )}
       </ul>
     </nav>
   );
-}
+};
 
 export default NavBar;
